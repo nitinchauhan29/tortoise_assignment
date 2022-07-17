4f4ce4e6e5a129eaf5546ffee4fcd7208ae3a16d
Nitin <nitin.chauhan@thoughts2binary.com>
2022-07-17 17:56:04 +0530

    making db changes

--- tortoise/admin.py
+++ tortoise/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Plans)
-admin.site.register(Promotions)
-admin.site.register(CustomerGoals)
+@admin.register(Plans)
+class PlanModelAdmin(admin.ModelAdmin):
+    list_display = ['planId', 'planName', 'amountOptions','tenureOptions', 'benefitPercentage','benefitType']
+
+@admin.register(Promotions)
+class PlanModelAdmin(admin.ModelAdmin):
+    list_display = ['promotionId', 'promotionType', 'promotionData','planId']
+
+
+@admin.register(CustomerGoals)
+class CustomersModelAdmin(admin.ModelAdmin):
+    list_display = ['goalId', 'userId', 'planId','selectedAmount', 'selectedTenure','startDate','depositedAmount']
